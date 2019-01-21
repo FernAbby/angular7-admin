8d65ff8101ffbfed54086f6999740b22922dc47b
wenzhen <zhen.wen@56qq.com>
2019-01-21 18:08:20 +0800

    feat: 临时提交

--- src/app/pages/demo/table/table-simple/table-simple.component.ts
+++ src/app/pages/demo/table/table-simple/table-simple.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-simple.component.scss']
 })
 export class TableSimpleComponent implements OnInit {
-
+  allChecked = false;
+  indeterminate = false;
+  displayData = [];
+  dataSet = [
+    {
+      key    : '1',
+      name   : 'John Brown',
+      age    : 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key    : '2',
+      name   : 'Jim Green',
+      age    : 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key    : '3',
+      name   : 'Joe Black',
+      age    : 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  currentPageDataChange($event: Array<{ name: string; age: number; address: string; checked: boolean; disabled: boolean; }>): void {
+    this.displayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    const allChecked = this.displayData.filter(value => !value.disabled).every(value => value.checked === true);
+    const allUnChecked = this.displayData.filter(value => !value.disabled).every(value => !value.checked);
+    this.allChecked = allChecked;
+    this.indeterminate = (!allChecked) && (!allUnChecked);
+  }
+
+  checkAll(value: boolean): void {
+    this.displayData.forEach(data => {
+      if (!data.disabled) {
+        data.checked = value;
+      }
+    });
+    this.refreshStatus();
+  }
 }
